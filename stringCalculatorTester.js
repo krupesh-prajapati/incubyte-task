87fd16f30fc6e6583fcd003911e610f0277cd984
krupesh <krupesh.prajapati@remedoapp.com>
2024-11-30 18:16:53 +0530

    Added more conditions

--- stringCalculatorTester.js
+++ stringCalculatorTester.js
@@ -12,6 +12,12 @@ function stringCalcTester(str) {
 
     if (stringCalculator('//;\n1;2') === 3) console.log('Test passed!')
     else console.log('Test failed...');
+
+    if (stringCalculator('-2,3,-5') === 'negative numbers not allowed -2,-5') console.log('Test passed!')
+    else console.log('Test failed...');
+
+    if (stringCalculator('2,1002') === 2) console.log('Test passed!')
+    else console.log('Test failed...');
 }
 
 module.exports = { stringCalcTester }
