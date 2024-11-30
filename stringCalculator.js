5fe8d3fd33e9ec8daf120ae46beaaa80638bb418
krupesh <krupesh.prajapati@remedoapp.com>
2024-11-30 17:51:50 +0530

    Added conditions

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,7 +1,36 @@
 
+function stringCalculator(str) {
 
-export function stringCalculator(str) {
 
     if (!str.length) return 0;
-        
+
+    let delimeter = ','
+
+    if (str.charAt(0) == '/' && str.charAt(1) == '/') {
+        delimeter = str.charAt(2);
+    }
+
+    const regexp = new RegExp(`${delimeter}|\n`, "g")
+
+    const arr = str.split(regexp);
+    let nagativeNums = [];
+    let res = 0;
+
+    arr.map((ele, idx) => {
+        if (!Number.isNaN(parseInt(ele))) {
+            if (parseInt(ele) < 0) {
+                nagativeNums.push(ele)
+            } else {
+                res += parseInt(ele)
+            }
+
+        }
+    })
+
+    return nagativeNums.length ? `negative numbers not allowed ${nagativeNums}` : res;
 }
+
+
+
+
+module.exports = { stringCalculator }
--- stringCalculatorTester.js
+++ stringCalculatorTester.js
@@ -1,6 +1,15 @@
-import { stringCalculator } from "./stringCalculator";
+const { stringCalculator } = require('./stringCalculator.js');
 
 function stringCalcTester(str) {
-    stringCalculator('') === 0 ? console.log('Test passed!') : console.log('Test failed...');
-    
+    if (stringCalculator('') === 0) console.log('Test passed!')
+    else console.log('Test failed...');
+
+    if (stringCalculator('2,3') === 5) console.log('Test passed!')
+    else console.log('Test failed...');
+
+    if (stringCalculator('1\n2,3') === 6) console.log('Test passed!')
+    else console.log('Test failed...');
+
 }
+
+module.exports = { stringCalcTester }
